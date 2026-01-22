e0dbdca2637fab41c6a219cf73bbf6e379413544
Jonathan Sabu <jonathansabu.cs24@jecc.ac.in>
2026-01-22 20:54:49 +0530

    Create Calpyth.py

---
+++ Calpyth.py
@@ -0,0 +1,16 @@
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    if b == 0:
+        return "Error: Division by zero"
+    return a / b
+
+print("Addition:", add(10, 5))
+print("Division:", divide(10, 2))
